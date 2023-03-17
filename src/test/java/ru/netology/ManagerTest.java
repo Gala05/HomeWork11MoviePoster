11aaffe92aee8fe18d1ae561c89020f2f2a04f89
Galina Mikhaylova <gala6858@mail.ru>
2023-03-17 20:08:53 +0300

    Add Repositoty&Test

---
+++ src/test/java/ru/netology/ManagerTest.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+//    Poster movie1 = new Poster(01, "film1", "animated film", "imageUrl1");
+//    Poster movie2 = new Poster(02, "film2", "action movie", "imageUrl2");
+//    Poster movie3 = new Poster(03, "film3", "comedy", "imageUrl3");
+//    Poster movie4 = new Poster(04, "film4", "horror", "imageUrl4");
+//
+//    @Test
+//    public void test() { //вывод всех по списку
+//        PosterRepository repo = new PosterRepository();
+//        repo.save(movie1);
+//        repo.save(movie2);
+//        repo.save(movie3);
+//        repo.save(movie4);
+//
+//        Poster[] expected = {movie1, movie2, movie3, movie4};
+//        Poster[] actual = repo.findAll();
+//
+//        Assertions.assertArrayEquals(expected, actual);
+//    }
+//    @Test
+//    public void test1() { //вывод всех по списку
+//        PosterRepository repo = new PosterRepository();
+//        repo.save(movie1);
+//        repo.save(movie2);
+//        repo.save(movie3);
+//        repo.save(movie4);
+//        Poster movie5 = new Poster(05, "film5", "horror", "imageUrl5");
+//        repo.save(movie5);
+//        Poster[] expected = {movie1, movie2, movie3, movie4, movie5};
+//        Poster[] actual = repo.findAll();
+//
+//        Assertions.assertArrayEquals(expected, actual);
+//    }
+//    @Test
+//    public void test2() { //найти по Id
+//        PosterRepository repo = new PosterRepository();
+//        repo.save(movie1);
+//        repo.save(movie2);
+//        repo.save(movie3);
+//        repo.save(movie4);
+//        repo.fineById(movie3.getId());
+//        Poster[] expected = {movie3};
+//        Poster[] actual = repo.findAll();
+//
+//        Assertions.assertArrayEquals(expected, actual);
+//    }
+}
